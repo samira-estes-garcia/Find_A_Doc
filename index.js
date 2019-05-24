a2d6a9589b0e30fdf10c9072e7e6cc51781b45a9
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-05-24 16:26:21 -0500

    finalized style added responsive

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ function findDoctor(specialization, docLocation, symp) {
     })
     .catch((error) => {
         console.log(error);
-        alert('Something went wrong, try again later');
     });
 }
 
@@ -31,9 +30,12 @@ function displayResults(results) {
     $('.container').css('display', 'none');
     $('html').css('background-image', 'none');
     $('body').css('background-image', 'none');
-    $('.main-nav').append(`<div>Perfom Another Search</div>`);
-    $('.display-doctors').html(`<h2>Here Are the Best Matches For You!</h2>`);
-    for (let i = 0; i < results.length; i++) { 
+    $('.main-nav').append(`<div><a href="index.html">Perfom Another Search</a></div>`);
+    if (results.length === 0) {
+        $('.display-doctors').append(`'<div class="no-result">Sorry, your search didn't return a match. <a href="index.html">Perform another search</a> to try again!</div>'`)
+    } else {
+        $('.display-doctors').html(`<h2>Here Are the Best Matches For You!</h2>`);
+        for (let i = 0; i < results.length; i++) { 
         $('.display-doctors').append(`
             <div class="doc-container">
             <div class="prof-img">
@@ -43,10 +45,11 @@ function displayResults(results) {
             <p><b>${results[i].practices[0].name}</b></p>
             <p><b>Speciality:</b> ${results[i].specialties[0].actor}</p>
             <p><b>Contact:</b> ${results[i].practices[0].phones[0].number}</p>
-            <p>${results[i].profile.bio}</p>
+            <p class="bio">${results[i].profile.bio}</p>
             </div>
             </div>
             `);
+        }
     }
 }
 
@@ -58,4 +61,21 @@ watchForm();
 //limit paginated
 //need more than one api call for each?? 
 //zipcode
+
+// $('.display-doctors').html(`<h2>Here Are the Best Matches For You!</h2>`);
+//     for (let i = 0; i < results.length; i++) { 
+//         $('.display-doctors').append(`
+//             <div class="doc-container">
+//             <div class="prof-img">
+//             <img src="${results[i].profile.image_url}" class="doc-img" alt="Image of doctor" />
+//             </div>
+//             <div class="prof-bio">
+//             <p><b>${results[i].practices[0].name}</b></p>
+//             <p><b>Speciality:</b> ${results[i].specialties[0].actor}</p>
+//             <p><b>Contact:</b> ${results[i].practices[0].phones[0].number}</p>
+//             <p class="bio">${results[i].profile.bio}</p>
+//             </div>
+//             </div>
+//             `);
+//     }
 
